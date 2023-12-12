5c5ca03f2981e85b7a35be8c4818e1f66315447d
ManhNguyen1903 <nguyenvanmanh1932003@gmail.com>
2023-12-12 10:03:25 +0700

    firt commit

---
+++ QuanLiChiTieuNhaTro/src/main/java/com/example/QuanLiChiTieuNhaTro/QuanLiChiTieuNhaTroApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuanLiChiTieuNhaTro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLiChiTieuNhaTroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLiChiTieuNhaTroApplication.class, args);
+	}
+
+}
---
+++ QuanLiChiTieuNhaTro/src/test/java/com/example/QuanLiChiTieuNhaTro/QuanLiChiTieuNhaTroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuanLiChiTieuNhaTro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLiChiTieuNhaTroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
