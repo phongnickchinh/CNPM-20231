b9070dc9dfc51c25bda29df2503a0d771782099c
Jenky Trần <tranvanluc.work@gmail.com>
2023-12-14 22:44:10 +0700

    Code du phong

---
+++ src/test/java/com/example/cnpm/quanlythuchinhatro/QuanLyThuChiNhaTroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cnpm.quanlythuchinhatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyThuChiNhaTroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
