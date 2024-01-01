3c23ead50117da6e5e50b82da7baf34048fa0e42
Jenky Trần <tranvanluc.work@gmail.com>
2024-01-01 18:44:35 +0700

    Fix http-only

--- src/main/java/com/example/cnpm/quanlythuchinhatro/QuanLyThuChiNhaTroApplication.java
+++ src/main/java/com/example/cnpm/quanlythuchinhatro/QuanLyThuChiNhaTroApplication.java
@@ -3,6 +3,8 @@ package com.example.cnpm.quanlythuchinhatro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class QuanLyThuChiNhaTroApplication {
@@ -10,5 +12,4 @@ public class QuanLyThuChiNhaTroApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuanLyThuChiNhaTroApplication.class, args);
     }
-
 }
