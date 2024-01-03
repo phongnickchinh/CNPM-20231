da764baca4e77932ad0e2e42b2467188f585355f
Jenky Trần <tranvanluc.work@gmail.com>
2024-01-03 14:45:39 +0700

    Điều chỉnh các api feeWithDeadline

--- src/main/java/com/example/cnpm/quanlythuchinhatro/repository/FeeWithDeadlineRepository.java
+++ src/main/java/com/example/cnpm/quanlythuchinhatro/repository/FeeWithDeadlineRepository.java
@@ -1,7 +1,10 @@
 package com.example.cnpm.quanlythuchinhatro.repository;
 
 import com.example.cnpm.quanlythuchinhatro.model.FeeWithDeadline;
+import com.example.cnpm.quanlythuchinhatro.model.UserFeeWithDeadline;
+import lombok.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- src/main/java/com/example/cnpm/quanlythuchinhatro/service/FeeWithDeadlineServiceImpl.java
+++ src/main/java/com/example/cnpm/quanlythuchinhatro/service/FeeWithDeadlineServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.cnpm.quanlythuchinhatro.service;
 
+import com.example.cnpm.quanlythuchinhatro.dto.FeeWithDeadlineDTO;
+import com.example.cnpm.quanlythuchinhatro.dto.UserDTO;
 import com.example.cnpm.quanlythuchinhatro.model.FeeWithDeadline;
+import com.example.cnpm.quanlythuchinhatro.model.User;
 import com.example.cnpm.quanlythuchinhatro.model.UserFeeWithDeadline;
 import com.example.cnpm.quanlythuchinhatro.repository.FeeWithDeadlineRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FeeWithDeadlineServiceImpl implements FeeWithDeadlineService{
@@ -31,10 +35,53 @@ public class FeeWithDeadlineServiceImpl implements FeeWithDeadlineService{
     }
 
     @Override
-    public FeeWithDeadline updateFeeWithDeadline(Integer id, FeeWithDeadline feeWithDeadline) {
-        feeWithDeadline.setId(id);
-        feeWithDeadlineRepository.save(feeWithDeadline);
-        return feeWithDeadline;
+    public FeeWithDeadline updateFeeWithDeadline(Integer id, FeeWithDeadlineDTO feeWithDeadlineDTO) {
+
+        Optional<FeeWithDeadline> feeWithDeadlineDB = feeWithDeadlineRepository.findById(id);
+
+        if (feeWithDeadlineDB.isPresent()) {
+            FeeWithDeadline fee = feeWithDeadlineDB.get();
+            if (feeWithDeadlineDTO.getStatus() != null) {
+                fee.setStatus(feeWithDeadlineDTO.getStatus());
+            }
+            if (feeWithDeadlineDTO.getDeadline() != null) {
+                fee.setDeadline(feeWithDeadlineDTO.getDeadline());
+            }
+            if(feeWithDeadlineDTO.getFeeName() != null) {
+                fee.setFeeName(feeWithDeadlineDTO.getFeeName());
+            }
+            if(feeWithDeadlineDTO.getMoney() != null) {
+                fee.setFeeName(feeWithDeadlineDTO.getFeeName());
+            }
+            if (feeWithDeadlineDTO.getRoomId() != null) {
+                fee.setRoomId(feeWithDeadlineDTO.getRoomId());
+            }
+
+            feeWithDeadlineRepository.save(fee);
+            return fee;
+        } else {
+            return null;
+        }
     }
+
+
+
+//    public User updateInfo(String username, UserDTO userDTO) {
+//
+//        Optional<User> userDB = userRepository.findByUsername(username);
+//
+//        if(userDB.isPresent()) {
+//            User user = userDB.get();
+//            if(userDTO.getPhoneNumber() != null ) {user.setPhoneNumber(userDTO.getPhoneNumber());}
+//            if(userDTO.getAvatarUrl() != null ) {user.setAvatarUrl(userDTO.getAvatarUrl());}
+//            if(userDTO.getBankName() != null ) {user.setBankName(userDTO.getBankName());}
+//            if(userDTO.getBankAccountNumber() != null ) {user.setBankAccountNumber(userDTO.getBankAccountNumber());}
+//
+//            userRepository.save(user);
+//            return user;
+//        } else {
+//            return null;
+//        }
+//    }
 
 }
