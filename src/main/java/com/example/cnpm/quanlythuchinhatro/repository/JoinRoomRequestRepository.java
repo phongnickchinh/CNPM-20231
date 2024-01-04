2aa82d0152bf03f611951a75369c02f965fcea62
Phạm Văn Phong <114666846+phonggiotai@users.noreply.github.com>
2024-01-04 17:10:09 +0700

    sửa fullName thành fullname

--- src/main/java/com/example/cnpm/quanlythuchinhatro/repository/JoinRoomRequestRepository.java
+++ src/main/java/com/example/cnpm/quanlythuchinhatro/repository/JoinRoomRequestRepository.java
@@ -10,7 +10,7 @@ import java.util.Map;
 @Repository
 public interface JoinRoomRequestRepository extends JpaRepository<JoinRoomRequest, Long> {
 
-    @Query("SELECT j.requestDate AS requestDate, j.userId AS userId, u.name AS fullName, u.avatarUrl AS avatarUrl FROM JoinRoomRequest j JOIN User u ON j.userId = u.id WHERE j.roomId =:roomId")
+    @Query("SELECT j.requestDate AS requestDate, j.userId AS userId, u.name AS fullname, u.avatarUrl AS avatarUrl FROM JoinRoomRequest j JOIN User u ON j.userId = u.id WHERE j.roomId =:roomId")
     List<Map<String, Object>> getJRRForAdmin(Integer roomId);
     //tim kiem join room request theo userId va roomId
     JoinRoomRequest findByUserIdAndRoomId(Integer userId, Integer roomId);
